70b8104262a3c3abb5b19b0004dfa280e04c7ee2
GaryGosh <clichethe1@yahoo.com>
2020-09-11 22:57:37 +0530

    bug fix

--- src/index.js
+++ src/index.js
@@ -21,20 +21,26 @@ app.post("/add", (req,res) => {
     const { num1, num2} = req.body;
 
     if(typeof num1 === "string" || typeof num2 === "string") {
-        res.json({
-            status: "failure",
+        return res.json({
+            status: "error",
             message: "invalid data types",
         });
     }
-    else if(num1 >= 1000000 || num2 >= 1000000 || num1+num2 > 1000000 ) {
-        res.json({
+    else if(num1 > 1000000 || num2 > 1000000 || (num1+num2) > 1000000 ) {
+        return res.json({
             status: "error",
             message: "Overflow",
         });
     }
+    else if(num1 <= 1000000 || num2 <= 1000000 || (num1+num2) <= 1000000 ) {
+        return res.json({
+            status: "error",
+            message: "Underflow",
+        });
+    }
     else {
         const sum = num1 + num2;
-        res.json({
+        return res.json({
             status: "success",
             message: "the sum of given two number",
             sum: `${sum}`,
@@ -46,21 +52,27 @@ app.post("/sub", (req, res) => {
     const { num1, num2 } = req.body;
 
     if(typeof num1 === "String" || typeof num2 === "string") {
-        res.json({
-            status: "failure",
+        return res.json({
+            status: "error",
             message: "invalid data types",
         });
     }
     else if(num1 < 1000000 || num2 < 1000000 || (num1-num2) < 1000000) {
-        res.json({
+        return res.json({
             status: "error",
             message: "Underflow"
+        });
+    }
+    else if(num1 > 1000000 || num2 > 1000000 || (num1-num2) > 1000000 ) {
+        return res.json({
+            status: "error",
+            message: "Overflow",
         });
     }
     else {
         const sum = num1 - num2;
 
-        res.json({
+        return res.json({
             status: "success",
             message: "the difference of given two number",
             sum: `${sum}`
@@ -72,20 +84,26 @@ app.post("/multiply", (req, res) => {
     const {num1, num2} = req.body;
 
     if(typeof num1 === "string" || typeof num2 === "string") {
-        res.json({
-            status: "failure",
+        return res.json({
+            status: "error",
             message: "invalid data types",
         });
     }
-    else if(num1 >= 1000000 || num2 >= 1000000 || (num1*num2) >= 1000000) {
-        res.json({
+    else if(num1 > 1000000 || num2 > 1000000 || (num1*num2) > 1000000) {
+        return res.json({
             status: "error",
             message: "Overflow",
         });
     }
+    else if(num1 < 1000000 || num2 < 1000000 || (num1*num2) < 1000000) {
+        return res.json({
+            status: "error",
+            message: "Underflow"
+        });
+    }
     else {
         const sum = num1 * num2;
-        res.json({
+        return res.json({
             status: "success",
             message: "The product of given numbers",
             sum: `${sum}`
@@ -97,20 +115,26 @@ app.post("/division", (req, res) => {
     const { num1, num2 } = req.body;
 
     if(typeof num1 === "string" || typeof num2 === "string") {
-        res.json({
-            status: "failure",
+        return res.json({
+            status: "error",
             message: "invalid data types",
         });
     }
     else if(num2 === 0) {
-        res.json({
+        return res.json({
             status: "error",
             message: "Cannot divide by zero",
         });
     }
+    else if((num1/num2) > 1000000) {
+        return res.json({
+            status: "error",
+            message: "Overflow",
+        })
+    }
     else {
         const sum = num1 / num2;
-        res.json({
+        return res.json({
             status: "success",
             message: "The division of given numbers",
             sum: `${sum}`
